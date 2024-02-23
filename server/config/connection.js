7787007f88c5c57e5bf6f91bd0b56886564ccc56
lufranc Kousse <kousselufranc477@gmail.com>
2024-02-23 00:32:45 -0600

    conection link changes final - 1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://lufranckousse:reacttask@cluster0.k0cfidq.mongodb.net/googlebooks?retryWrites=true&appName=Cluster0";
+const uri = "mongodb+srv://lufranckousse:reacttask@cluster0.k0cfidq.mongodb.net/googlebooks";
 
 mongoose.connect(
   
-  
+
   //process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
   process.env.MONGODB_URI || uri,
   {
